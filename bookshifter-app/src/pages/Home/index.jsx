e6cd9415cf08de5cedfd17d33e81d8f95ddaf565
Aluno <Aluno@fatecdiadema.com>
2024-04-17 08:57:56 -0300

    Refatorando a página inicial e arrumando responsividade

--- bookshifter-app/src/pages/Home/index.jsx
+++ bookshifter-app/src/pages/Home/index.jsx
@@ -7,11 +7,11 @@ import imgHome from "../../img/img-home.png";
 
 function Home() {
   return (
-    <div className="container mx-auto p-10">
+    <div className="container mx-auto">
       {/* Home */}
       <section className="xl:my-40 my-20">
-        <div className="xl:relative xl:h-[400px]">
-          <div className=" xl:bg-gray-100 xl:bg-opacity-75 xl:backdrop-blur-md xl:max-w-[500px] xl:w-[550px] xl:h-[290px] xl:py-2 xl:px-10 xl:rounded-md xl:z-30 xl:absolute xl:top-[70px] xl:left-[250px]">
+        <div className="xl:relative xl:h-[400px] px-10 py-5">
+          <div className="xl:bg-gray-100 xl:bg-opacity-75 xl:backdrop-blur-md xl:max-w-[500px] xl:w-[550px] xl:h-[290px] xl:py-2 xl:px-10 xl:rounded-md xl:z-30 xl:absolute xl:top-[70px] xl:left-[130px] 2xl:left-[250px]">
             <h1 className="xl:text-5xl font-extrabold mb-4 mt-4 text-3xl text-center xl:text-left md:text-4xl md:text-left">
               Doe seus livros e transforme vidas
             </h1>
@@ -19,14 +19,14 @@ function Home() {
               Descubra como o Bookshifter está revolucionando a educação e o
               meio ambiente através da doação de livros.
             </div>
-          <button className="bg-buttonColor text-white xl:mt-1 py-2 px-4 rounded-md mb-5 w-full sm:w-auto xl:w-auto md:px-16">
+          <button className="bg-buttonColor text-white xl:mt-1 py-2 px-4 rounded-md mb-5 w-full sm:w-auto xl:w-auto md:px-16 hover:bg-secondaryColor">
             Doar agora
           </button>
           </div>
-          <div className="xl:absolute xl:top-0 xl:left-[700px] xl:w-[530px] xl:h-[380px] xl:py-2 xl:px-4 rounded-md xl:z-20">
+          <div className="xl:absolute xl:top-0 xl:left-[580px] 2xl:left-[700px] xl:w-[530px] xl:h-[380px] xl:py-2 xl:px-4 rounded-md xl:z-20">
             <img src={imgHome} alt="capa do livro" className="w-full h-full" />
           </div>
-          <div className="xl:bg-secondaryColor xl:absolute xl:top-[55px] xl:left-[680px] xl:w-[500px] xl:h-[350px] py-2 px-4 rounded-md z-10"></div>
+          <div className="xl:bg-secondaryColor xl:absolute xl:top-[55px] xl:left-[570px] 2xl:left-[680px] xl:w-[500px] xl:h-[350px] py-2 px-4 rounded-md z-10"></div>
         </div>
       </section>
 
@@ -50,11 +50,11 @@ function Home() {
       </section>
 
       {/* Beneficios */}
-      <section className="my-20">
+      <section className="xl:my-20 px-10 xl:px-40">
         <span className="font-base text-buttonColor font-extrabold">
           Benefícios
         </span>
-        <h2 className="xl:text-5xl font-extrabold mb-16 mt-4 text-3xl md:text-4xl">
+        <h2 className="xl:text-5xl font-extrabold xl:mb-16 mb-8 mt-4 text-3xl md:text-4xl">
           Benefícios ao utilizar nossos serviços
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
@@ -103,15 +103,15 @@ function Home() {
       </section>
 
       {/* Como Funciona */}
-      <section>
+      <section className="px-10 xl:px-40 mt-5">
         <span className="font-base text-buttonColor font-extrabold">
           Como funciona
         </span>
-        <h2 className="xl:text-5xl text-3xl font-extrabold mb-16 mt-4 md:text-4xl">Como Funciona</h2>
+        <h2 className="xl:text-5xl text-3xl font-extrabold xl:mb-16 mb-8 mt-4 md:text-4xl">Como Funciona</h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
           <div className="sm:flex sm:w-full mb-5">
             <div>
-              <span className="font-extrabold text-baseColor xl:text-6xl text-4xl">01</span>
+              <span className="font-extrabold text-baseColor 2xl:text-6xl xl:text-5xl text-4xl">01</span>
               <h3 className="text-2xl font-bold leading-none text-blackColor mt-8 mb-4">
                 Cadastro
               </h3>
@@ -122,7 +122,7 @@ function Home() {
           </div>
           <div className="sm:flex sm:w-full mb-5">
             <div>
-              <span className="font-extrabold text-baseColor xl:text-6xl text-4xl">02</span>
+              <span className="font-extrabold text-baseColor 2xl:text-6xl xl:text-5xl text-4xl">02</span>
               <h3 className="text-2xl font-bold leading-none text-blackColor mt-8 mb-4">
                 Listagem
               </h3>
@@ -134,7 +134,7 @@ function Home() {
           </div>
           <div className="sm:flex sm:w-full mb-5">
             <div>
-              <span className="font-extrabold text-baseColor xl:text-6xl text-4xl">03</span>
+              <span className="font-extrabold text-baseColor 2xl:text-6xl xl:text-5xl text-4xl">03</span>
               <h3 className="text-2xl font-bold leading-none text-blackColor mt-8 mb-4">
                 Busca
               </h3>
@@ -145,7 +145,7 @@ function Home() {
           </div>
           <div className="sm:flex sm:w-full mb-5">
             <div>
-              <span className="font-extrabold text-baseColor xl:text-6xl text-4xl">04</span>
+              <span className="font-extrabold text-baseColor 2xl:text-6xl xl:text-5xl text-4xl">04</span>
               <h3 className="text-2xl font-bold leading-none text-blackColor mt-8 mb-4">
                 Reserva
               </h3>
@@ -154,7 +154,7 @@ function Home() {
           </div>
           <div className="sm:flex sm:w-full mb-5">
             <div>
-              <span className="font-extrabold text-baseColor xl:text-6xl text-4xl">05</span>
+              <span className="font-extrabold text-baseColor 2xl:text-6xl xl:text-5xl text-4xl">05</span>
               <h3 className="text-2xl font-bold leading-none text-blackColor mt-8 mb-4">
                 Localize
               </h3>
@@ -165,7 +165,7 @@ function Home() {
           </div>
           <div className="sm:flex sm:w-full mb-5">
             <div>
-              <span className="font-extrabold text-baseColor xl:text-6xl text-4xl">06</span>
+              <span className="font-extrabold text-baseColor 2xl:text-6xl xl:text-5xl text-4xl">06</span>
               <h3 className="text-2xl font-bold leading-none text-blackColor mt-8 mb-4">
                 Retirada
               </h3>
